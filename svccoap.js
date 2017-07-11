e21df980548e51db7e86ff1c8cb8205cf7197f7a
glenn west <glennswest@neuralcloudcomptuing.com>
2017-07-11 15:24:03 +0800

    Add temp just for test

--- svccoap.js
+++ svccoap.js
@@ -15,12 +15,19 @@ server.on('request', function(req, res) {
   console.log(util.inspect(req));
   urlparts = req.url.split('/');
   switch(urlparts[1]){
+     case 'temperature':
+          topic = urlparts[2];
+          console.log("Topic: " + topic);
+          var message = req.payload.toString();
+          console.log(conv(message, {in:'hex'}));
+          client.publish(topic,message);
+          break;
      case 'mqtt':
           topic = urlparts[2];
           console.log("Topic: " + topic);
           var message = req.payload.toString();
           console.log(conv(message, {in:'hex'}));
-          client.public('topic',message);
+          client.publish(topic,message);
           break;
      default:
           console.log("Invalid Message: " + urlparts[1]);
